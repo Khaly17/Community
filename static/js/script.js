d86527828d7a6c4c3894be3518023fb68ba8f50b
Khaly17 <diengkhaly17@gmail.com>
2021-08-22 07:24:55 +0000

    ajoute de la fonctionnalite mode dark

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,36 @@
-const readMoreBtn = document.querySelector(".read-more-btn");
-const text = document.querySelector(".text");
+const toggleSwitch = document.querySelector('input[type = "checkbox"]');
+const doc = document.getElementsByTagName('html')[0];
+const bg = document.getElementById('torch');
 
-readMoreBtn.addEventListener("click", (e) => {
-    text.classList.toggle("show-more");
-    if (readMoreBtn.innerText === "Read More") {
-        readMoreBtn.innerText = "Read Less";
-    } else {
-        readMoreBtn.innerText = "Read More";
-    }
-});
+
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    doc.className = 'dark'
+    bg.className = "bg-black dark:bg-black"
+
+
+    toggleSwitch.checked = true;
+  } else {
+    doc.className = 'light'
+    bg.className = "bg-white dark:bg-black"
+
+    //document.documentElement.classList.remove('dark')
+  }
+
+  function switchTheme(){
+      if(toggleSwitch.checked){
+        localStorage.setItem('theme','dark');
+        doc.className = 'dark'
+        bg.className = "bg-black dark:bg-black"
+      }
+      else{
+        localStorage.setItem('theme','light');
+        doc.className = 'light'
+        bg.className = "bg-white dark:bg-black"
+      }
+  }
+
+
+  toggleSwitch.addEventListener('click',switchTheme,false);
 
